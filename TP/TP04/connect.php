4da303fb7564474dec51abbdb4d7cbfe0bda3971
Alexandre Masse <alexandre.masse94@gmail.com>
2017-03-30 14:29:06 -0400

    clean repository to keep only Symfony blog project

--- TP/TP04/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//$bdd = new PDO('mysql:host=localhost;dbname=developpement_web;charset=utf8', 'root', '');
-
-
-$connexion=mysqli_connect("localhost","root","");
-mysqli_select_db($connexion, "developpement_web_TP04");
-
-?>
